505e5293fd9c46ca56be6146564a5ef317346c05
VladTca <votsats@gmail.com>
2024-05-26 17:11:50 +0200

    🎨 Improve structure / format of the code.

--- healthArticle/health_article.js
+++ healthArticle/health_article.js
@@ -1,48 +1,55 @@
 var xhr = new XMLHttpRequest();
-var url = './health.json';
+var url = './health_article.json';
 xhr.open('GET', url, true);
 xhr.responseType = 'json';
-xhr.onload = function() {
-    var articles = xhr.response.articles;
-var articlesDiv = document.getElementById('articles');
- }
- articles.forEach(function(article) {
-    var articleDiv = document.createElement('div');
-    articleDiv.classList.add('article');
 
-    var title = document.createElement('h2');
-    title.textContent = article.title;
+xhr.onload = function () {
+    if (xhr.status === 200) {
+        var articles = xhr.response.articles;
+        var articlesDiv = document.getElementById('articles');
 
-    var description = document.createElement('p');
-    description.textContent = article.description;
+        articles.forEach(function (article) {
+            var articleDiv = document.createElement('div');
+            articleDiv.classList.add('article');
 
-    var waysHeader = document.createElement('h3');
-    waysHeader.textContent = 'Ways to Achieve:';
+            var title = document.createElement('h2');
+            title.textContent = article.title;
 
-    var waysList = document.createElement('ul');
-    article.ways_to_achieve.forEach(function(way) {
-      var listItem = document.createElement('li');
-      listItem.textContent = way;
-      waysList.appendChild(listItem);
-    });
+            var description = document.createElement('p');
+            description.textContent = article.description;
 
-    var benefitsHeader = document.createElement('h3');
-    benefitsHeader.textContent = 'Benefits:';
+            var waysHeader = document.createElement('h3');
+            waysHeader.textContent = 'Ways to Achieve:';
 
-    var benefitsList = document.createElement('ul');
-    article.benefits.forEach(function(benefit) {
-      var listItem = document.createElement('li');
-      listItem.textContent = benefit;
-      benefitsList.appendChild(listItem);
-    });
+            var waysList = document.createElement('ul');
+            article.ways_to_achieve.forEach(function (way) {
+                var listItem = document.createElement('li');
+                listItem.textContent = way;
+                waysList.appendChild(listItem);
+            });
 
-    articleDiv.appendChild(title);
-    articleDiv.appendChild(description);
-    articleDiv.appendChild(waysHeader);
-    articleDiv.appendChild(waysList);
-    articleDiv.appendChild(benefitsHeader);
-    articleDiv.appendChild(benefitsList);
+            var benefitsHeader = document.createElement('h3');
+            benefitsHeader.textContent = 'Benefits:';
 
-    articlesDiv.appendChild(articleDiv);
-  });
-  xhr.send();
+            var benefitsList = document.createElement('ul');
+            article.benefits.forEach(function (benefit) {
+                var listItem = document.createElement('li');
+                listItem.textContent = benefit;
+                benefitsList.appendChild(listItem);
+            });
+
+            articleDiv.appendChild(title);
+            articleDiv.appendChild(description);
+            articleDiv.appendChild(waysHeader);
+            articleDiv.appendChild(waysList);
+            articleDiv.appendChild(benefitsHeader);
+            articleDiv.appendChild(benefitsList);
+
+            articlesDiv.appendChild(articleDiv);
+        });
+    } else {
+        console.error('Failed to load articles. Status:', xhr.status);
+    }
+};
+
+xhr.send();
